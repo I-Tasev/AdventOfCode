6d3b600cf403a53b170b52fe43123f279b57fa04
I-Tasev <ivan.tassev.92@gmail.com>
2023-12-01 11:45:46 +0200

    Day 1 Part 1 Working solution

--- AdventOfCode-Day1-1/Program.cs
+++ AdventOfCode-Day1-1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Please enter calibration document!");
+
+List<String> input = new List<string>();
+
+var oneLineInput = Console.ReadLine();
+
+while (oneLineInput != string.Empty)
+{
+    input.Add(oneLineInput);
+    oneLineInput = Console.ReadLine();
+}
+
+var calibrationSum = 0;
+
+foreach (var line in input)
+{
+    IEnumerable<char> stringQuery =
+         from ch in line
+         where Char.IsDigit(ch)
+         select ch;
+    char[] chars = { (char)stringQuery.FirstOrDefault(), (char)stringQuery.LastOrDefault() };
+    var calibrationValueString = new string(chars);
+    var calibrationValue = int.Parse(calibrationValueString);
+    calibrationSum += calibrationValue;
+}
+
+Console.WriteLine(calibrationSum);
