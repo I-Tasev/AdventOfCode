b4dd88bee7317af9c137fb874078d12267062829
I-Tasev <ivan.tassev.92@gmail.com>
2023-12-04 09:42:35 +0200

    Add Day 4 part 1 solution

---
+++ Day04-1/Day04-1.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Please enter game input and press Enter!");
+
+List<String> input = new List<string>();
+var oneLineInput = Console.ReadLine();
+
+while (oneLineInput != string.Empty)
+{
+    input.Add(oneLineInput);
+    oneLineInput = Console.ReadLine();
+}
+
+var totalPoints = 0;
+
+foreach (var line in input)
+{
+    var cardID = line.Split(':').FirstOrDefault();
+    var allNumbers = line.Split(":").LastOrDefault();
+    var winningNumbers = allNumbers.Split('|').FirstOrDefault().Trim();
+    var numbers = allNumbers.Split('|').LastOrDefault().Trim();
+    var winningNumbersList = winningNumbers.Split(" ", System.StringSplitOptions.RemoveEmptyEntries).ToList();
+    var numbersList = numbers.Split(" ", System.StringSplitOptions.RemoveEmptyEntries).ToList();
+    var cardPoits = 0;
+
+    foreach (var winNumber in winningNumbersList)
+    {
+        if (numbersList.Contains(winNumber))
+        {
+            if (cardPoits == 0)
+            {
+                cardPoits = 1;
+            }
+            else
+            {
+                cardPoits *= 2;
+            }
+        }
+    }
+    totalPoints += cardPoits;
+    Console.WriteLine($"{cardID} --- {cardPoits}");
+}
+
+Console.WriteLine(totalPoints);
