e3ac052b0bec8433c41eae81aaf7fa21e8b4f918
I-Tasev <ivan.tassev.92@gmail.com>
2023-12-09 11:28:21 +0200

    Solution for day 9 part 2

---
+++ Day09-2/Day09-2.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+
+Console.WriteLine("Please enter game input and press Enter!");
+
+List<String> input = new List<string>();
+var oneLineInput = Console.ReadLine();
+
+while (oneLineInput != string.Empty)
+{
+    input.Add(oneLineInput);
+    oneLineInput = Console.ReadLine();
+}
+
+var sum = 0;
+
+foreach (var history in input)
+{
+    var historyValues = history.Split().Select(n => int.Parse(n)).ToList();
+    var historyPyramid = new List<List<int>>();
+    historyPyramid.Add(historyValues);
+
+    var currentLine = historyValues;
+
+    while (!currentLine.All(n => n == 0))
+    {
+        var nextLine = new List<int>();
+
+        for (int i = 0; i < currentLine.Count - 1; i++)
+        {
+            nextLine.Add(currentLine[i + 1] - currentLine[i]);
+        }
+
+        currentLine = nextLine;
+
+        historyPyramid.Add(currentLine);
+    }
+
+    foreach (var line in historyPyramid)
+    {
+        var lineString = "";
+        foreach (var num in line)
+        {
+            lineString += $"{num} ";
+        }
+        Console.WriteLine(lineString);
+    }
+    var firstElemntsFromZero = historyPyramid.Select(x => x.FirstOrDefault()).Reverse();
+
+    var newValue = 0;
+    foreach (var element in firstElemntsFromZero)
+    {
+        newValue = element - newValue;
+    }
+
+    sum+= newValue;
+}
+
+Console.WriteLine("---------");
+Console.WriteLine("---------");
+Console.WriteLine(sum);
